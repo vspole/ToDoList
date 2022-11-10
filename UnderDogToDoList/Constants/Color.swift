823d5f9b596db226edf1a9233a9a3e8ca32c1366
Vishal Polepalli <vishal.polepalli@gmail.com>
2022-11-10 12:41:15 -0600

    Added constants and scaled font extension

---
+++ UnderDogToDoList/Constants/Color.swift
@@ -0,0 +1,10 @@
+//
+//  Color.swift
+//  UnderDogToDoList
+//
+//  Created by Vishal Polepalli on 11/10/22.
+//
+
+import SwiftUI
+
+let ACCENT_COLOR = Color("AccentColor")
---
+++ UnderDogToDoList/Extensions/ScaledFont.swift
@@ -0,0 +1,55 @@
+//
+//  ScaledFont.swift
+//  UnderDogToDoList
+//
+//  Created by Vishal Polepalli on 11/10/22.
+//
+
+import SwiftUI
+
+struct ScaledFont: ViewModifier {
+    @Environment(\.sizeCategory) var sizeCategory
+    var name: String
+    var size: CGFloat
+    var color: Color?
+
+    @ViewBuilder func body(content: Content) -> some View {
+        let scaledSize = UIFontMetrics.default.scaledValue(for: size)
+
+        if let color = color {
+            content
+                .font(.custom(name, size: scaledSize))
+                .foregroundColor(color)
+        } else {
+            content
+                .font(.custom(name, size: scaledSize))
+        }
+    }
+}
+
+enum ScaledFontType: String {
+    // https://fonts.google.com/specimen/Open+Sans
+    case openSansRegular = "OpenSans-Regular"
+    case openSansSemiBold = "OpenSans-Semibold"
+    case openSansBold = "OpenSans-Bold"
+    case quickSandMedium = "Quicksand-Medium"
+    case quickSandBold = "Quicksand-Bold"
+    case quickSandLight = "Quicksand-Light"
+    case quickSandRegular = "Quicksand-Regular"
+    case quickSandSemiBold = "Quicksand-SemiBold"
+
+}
+
+extension View {
+    func scaledFont(type: ScaledFontType = .openSansRegular, size: CGFloat = 17, color: Color? = nil) -> some View {
+        modifier(ScaledFont(name: type.rawValue, size: size, color: color))
+    }
+}
+
+extension UIFont {
+    static func scaledFont(type: ScaledFontType = .openSansRegular, size: CGFloat = 17) -> UIFont {
+        let font = UIFont(name: type.rawValue, size: size) ?? .systemFont(ofSize: size)
+        return UIFontMetrics.default.scaledFont(for: font)
+    }
+}
+
