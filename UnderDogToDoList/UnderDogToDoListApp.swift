30297c5d8adef39e78e6374fa4055e1d7784c92c
Vishal Polepalli <vishal.polepalli@gmail.com>
2022-11-10 10:50:54 -0600

    Initial Commit

---
+++ UnderDogToDoList/UnderDogToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderDogToDoListApp.swift
+//  UnderDogToDoList
+//
+//  Created by Vishal Polepalli on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct UnderDogToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
